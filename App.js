626ad86abfac34b1077a617faa5b3a082cd378f1
Rahulraj9936 <rahulraj731111@gmail.com>
2022-07-15 12:52:31 +0530

    bug fix

--- App.js
+++ App.js
@@ -229,9 +229,9 @@ export default function App() {
               <View>
                 <Image source={voucher}
                   resizeMode='contain'
-                  style={{ width: normalize(35), height: normalize(35), }}
+                  style={{ width: normalize(35), height: normalize(35),marginLeft:10 }}
                 />
-                <Text style={{ textAlign: 'center', fontSize: normalize(12), fontFamily: fonts.inter_bold, color: "#ffffff", right : 5, letterSpacing : 1 }}>{"Voucher"}</Text>
+                <Text style={{ textAlign: 'center', fontSize: normalize(12), fontFamily: fonts.inter_bold, color: "#ffffff", letterSpacing : 1 }}>{"Voucher"}</Text>
               </View>
             )
           }}
@@ -248,7 +248,7 @@ export default function App() {
               <View>
                 <Image source={cart}
                   resizeMode='contain'
-                  style={{ width: normalize(35), height: normalize(35), marginRight:9}}
+                  style={{ width: normalize(35), height: normalize(35), marginLeft:2}}
                 />
                 <Text style={{ textAlign: 'center', fontSize: normalize(12), fontFamily: fonts.inter_bold, color: "#ffffff", letterSpacing : 1 }}>{"Cart"}</Text>
               </View>
